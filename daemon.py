92e9a8bb426bb0c22be9ea5527211e32d365f221
Florian Schüller <florian.schueller@gmail.com>
2017-03-12 12:09:06 +0100

    ignore problems when sending fails while connection is bad...

--- daemon.py
+++ daemon.py
@@ -36,7 +36,11 @@ while True:
         # TODO error handling
             print(result)
     except (ValueError, requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as err:
-        methods.sendToAll("Connection Error {0}\nRetrying in {1} seconds".format(err, server_retry))
+        # really just _try_ to inform others about _connection problems_ ... :)
+        try:
+            methods.sendToAll("Connection Error {0}\nRetrying in {1} seconds".format(err, server_retry))
+        except:
+            pass
         time.sleep(server_retry)
         server_retry *= 2
 
